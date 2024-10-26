bc67871343de10fcb29143e24b6ba501ac502455
dev-from-cbd <veryfatwombat@gmail.com>
2024-10-27 06:50:45 +1030

    React JS

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
   return (
     <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Web App
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span class="navbar-toggler-icon"></span>
           </button>
-        </nav>
+
+<button className="btn btn-"/>
+
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
