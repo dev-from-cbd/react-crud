61982465625133c22d812f58057396fbd5ee176a
dev-from-cbd <veryfatwombat@gmail.com>
2024-10-30 06:35:14 +1030

    React JS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+// Importing the CSS file for styling the App component.
 import "./App.css";
+
+// Importing Bootstrap CSS for pre-built styles, like buttons and layout.
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+// Importing the Navbar component from the layout directory.
 import Navbar from "./layout/Navbar";
 
+// Defining the main App component.
 function App() {
   return (
+    // Creating a div with the class name 'App' to contain the components of the app.
     <div className="App">
+      {/* Rendering the Navbar component inside the App */}
       <Navbar />
     </div>
   );
 }
 
+// Exporting the App component as the default export so it can be used elsewhere in the project.
 export default App;
