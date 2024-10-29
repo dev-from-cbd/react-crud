a05016114c88814aecc4502a1cdffe0715e48a58
dev-from-cbd <veryfatwombat@gmail.com>
2024-10-29 20:44:43 +1030

    React JS

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,13 +1,18 @@
+// Import React library
 import React from "react";
 
+// Define and export the Navbar component
 export default function Navbar() {
   return (
     <div>
+      {/* Navigation bar with Bootstrap classes */}
       <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
+          {/* Brand name with a link */}
           <a class="navbar-brand" href="#">
             Web App
           </a>
+          {/* Button to toggle the navigation menu in smaller screens */}
           <button
             class="navbar-toggler"
             type="button"
@@ -20,6 +25,7 @@ export default function Navbar() {
             <span class="navbar-toggler-icon"></span>
           </button>
 
+          {/* Button to add a new user */}
           <button className="btn btn-outline-light">Add user</button>
         </div>
       </nav>
