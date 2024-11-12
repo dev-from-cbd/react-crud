5eeb7e903d53c2750736b4b746a6f4a05b8f114d
dev-from-cbd <veryfatwombat@gmail.com>
2024-11-12 18:23:46 +1100

    React JS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,56 +1,43 @@
-import React, { useState, useEffect } from "react"; // Import the React library
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-// Export the Home component as the default export
 export default function Home() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
+  useEffect(() => {});
 
-  });
-
-  const loadUsers = () => {
-  
-  };
+  const loadUsers = () => {};
 
   return (
-    // Container div with a Bootstrap class for centering and spacing
     <div className="container">
-      {/* A div with padding, using Bootstrap's py-4 class for vertical spacing */}
       <div className="py-4">
-        {/* Create a table using Bootstrap's table class for styling */}
         <table className="table border shadow">
-          {/* Table head that contains the column names */}
           <thead>
             <tr>
-              {/* Column headers with scope attribute for accessibility */}
-              <th scope="col">#</th> {/* Column header for row numbers */}
-              <th scope="col">First</th> {/* Column header for first name */}
-              <th scope="col">Last</th> {/* Column header for last name */}
-              <th scope="col">Handle</th>{" "}
-              {/* Column header for social media handle */}
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
             </tr>
           </thead>
-          {/* Table body containing the actual data rows */}
           <tbody>
             <tr>
-              <th scope="row">1</th> {/* Row number 1 */}
-              <td>Markus</td> {/* First name of the first row */}
-              <td>Otto</td> {/* Last name of the first row */}
-              <td>@mdo</td> {/* Handle of the first row */}
+              <th scope="row">1</th>
+              <td>Markus</td>
+              <td>Otto</td>
+              <td>@mdo</td>
             </tr>
             <tr>
-              <th scope="row">2</th> {/* Row number 2 */}
-              <td>Jacob</td> {/* First name of the second row */}
-              <td>Thornton</td> {/* Last name of the second row */}
-              <td>@fat</td> {/* Handle of the second row */}
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
             </tr>
             <tr>
-              <th scope="row">3</th> {/* Row number 3 */}
-              <td>John</td> {/* First name of the second row */}
-              <td>Doe</td> {/* Last name of the second row */}
-              {/*<td colspan="2">Larry the Bird</td> Name of the third row spanning two columns */}
-              <td>@twitter</td> {/* Handle of the third row */}
+              <th scope="row">3</th>
+              <td>John</td>
+              <td>Doe</td>
+              <td>@twitter</td>
             </tr>
           </tbody>
         </table>
