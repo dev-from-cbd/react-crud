b49ee5619e9065787f7a807f681965923517fe16
dev-from-cbd <veryfatwombat@gmail.com>
2024-11-18 06:07:14 +1030

    React JS

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
 
   useEffect(() => {
     loadUsers();
-  });
+  },[]);
 
   const loadUsers = async () => {
     const result = await axios.get("http://localhost:3000/users");
