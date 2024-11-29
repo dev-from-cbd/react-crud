3c89de8581f2c7e0242ed6f6862c704c9d6e10b4
dev-from-cbd <veryfatwombat@gmail.com>
2024-11-30 09:33:13 +1030

    Add User

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        <Home />
 
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="adduser" element={<AddUser />} />
         </Routes>
       </Router>
     </div>
