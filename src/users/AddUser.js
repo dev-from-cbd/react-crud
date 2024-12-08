297d02548405d6e8a87fd2f52d1e970c823f4b45
dev-from-cbd <veryfatwombat@gmail.com>
2024-12-09 06:28:07 +1030

    CSS3

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -4,7 +4,7 @@ export default function () {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-6 offset-md-3 border rounded p-4">row</div>
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2">row</div>
       </div>
     </div>
   );
