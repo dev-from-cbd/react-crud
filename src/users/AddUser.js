f2b08f3dc138ae34946bbc12bd91dd7cc7e0caa5
dev-from-cbd <veryfatwombat@gmail.com>
2024-12-10 06:35:00 +1030

    CSS3

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -4,7 +4,9 @@ export default function () {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2">row</div>
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Register User</h2>
+        </div>
       </div>
     </div>
   );
