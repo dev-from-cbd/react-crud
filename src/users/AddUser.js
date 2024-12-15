cac60840e01791f1e14299b58a230d3627e0eedb
dev-from-cbd <veryfatwombat@gmail.com>
2024-12-16 05:44:25 +1030

    React JS

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -1,17 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AddUser() {
+  const [formData, setFormData] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">Register User</h2>
-          <div className="mb-3">
-            <label htmlFor="Name" className="form-label">
-              Name
-            </label>
-            <input type={text} className="form-control" />
-          </div>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                Name
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your name"
+                name="name"
+                id="name"
+                value={formData.name}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">
+                Username
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your Username"
+                name="username"
+                id="username"
+                value={formData.username}
+                onChange={handleChange}
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                Email
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter your Email Address"
+                name="email"
+                id="email"
+                value={formData.email}
+                onChange={handleChange}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary w-100">
+              Register
+            </button>
+          </form>
         </div>
       </div>
     </div>
