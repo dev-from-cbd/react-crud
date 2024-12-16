c136180a009b91a4d5b4ceace9d435a022e3a9dc
dev-from-cbd <veryfatwombat@gmail.com>
2024-12-17 06:40:43 +1100

    React JS

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -68,6 +68,9 @@ export default function AddUser() {
             <button type="submit" className="btn btn-outline-primary">
               Submit
             </button>
+            <button type="submit" className="btn btn-outline-danger mx-2">
+              Cancel
+            </button>
           </form>
         </div>
       </div>
