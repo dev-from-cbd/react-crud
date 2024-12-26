12b7c4057e22751a8e92e7916718c0cba0a76426
dev-from-cbd <veryfatwombat@gmail.com>
2024-12-27 07:13:20 +1030

    React JS

--- src/users/AddUser.js
+++ src/users/AddUser.js
@@ -19,6 +19,7 @@ export default function AddUser() {
   const onSubmit = async (e) => {
     e.preventDefault();
     await axios.post("http://localhost:8080/user", user);
+    navigate("/");
   };
 
   return (
