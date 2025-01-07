eddc2463a77069c504e8e1b05e5a3e73a9057fb3
dev-from-cbd <veryfatwombat@gmail.com>
2025-01-08 07:25:18 +1030

    React JS

--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default function 
-() {
+export default function () {
   return (
-    <div>
-        
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+          <h2 className="text-center m-4">Edit User</h2>
+        </div>{" "}
+      </div>{" "}
     </div>
-  )
+  );
 }
