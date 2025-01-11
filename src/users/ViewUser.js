375cc93ff6febdd08fce6499cb1207f25a890115
dev-from-cbd <veryfatwombat@gmail.com>
2025-01-12 08:35:44 +1030

    React JS

--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -6,6 +6,18 @@ export default function ViewUser() {
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
           <h2 className="text-center m-4">User Details</h2>
+          <div сlassName="card">
+            <div className="card-header">
+              Details of User ID:
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Name: </li>
+                <li className="list-group-item">Username: </li>
+                <li className="list-group-item">Email: </li>
+                <li className="list-group-item">Phone: </li>
+                <li className="list-group-item">Website: </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
