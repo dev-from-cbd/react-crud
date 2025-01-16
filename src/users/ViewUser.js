ae373a0eb7c0c66adddbead8324540da3d8f50ba
dev-from-cbd <veryfatwombat@gmail.com>
2025-01-17 06:29:35 +1100

    Update ViewUser.js

--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function ViewUser() {
+  const [user, setUser] = useState({});
   return (
     <div className="container">
       <div className="row">
