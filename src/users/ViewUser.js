d098accbb1807a31ca0d6b6e778aa64bf204e62d
dev-from-cbd <veryfatwombat@gmail.com>
2025-01-25 07:18:35 +1030

    React JS

--- src/users/ViewUser.js
+++ src/users/ViewUser.js
@@ -17,6 +17,7 @@ export default function ViewUser() {
 
   const loadUser = async () => {
     const result = await axios.get(`http://localhost:8080/users/${id}`);
+    setUser(result.data);
   };
 
   return (
