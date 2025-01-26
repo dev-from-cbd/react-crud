e36f24d8f69aa003d31c3d241ebc586b6c085f8a
dev-from-cbd <veryfatwombat@gmail.com>
2025-01-27 07:51:54 +1030

    React JS

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,20 +1,16 @@
-// Import React library
 import React from "react";
+import { Link } from "react-router-dom";
 
-// Define and export the Navbar component
 export default function Navbar() {
   return (
     <div>
-      {/* Navigation bar with Bootstrap classes */}
-      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-          {/* Brand name with a link */}
-          <a class="navbar-brand" href="#">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
             Web App
-          </a>
-          {/* Button to toggle the navigation menu in smaller screens */}
+          </Link>
           <button
-            class="navbar-toggler"
+            className="navbar-toggler"
             type="button"
             data-bs-toggle="collapse"
             data-bs-target="#navbarSupportedContent"
@@ -22,11 +18,11 @@ export default function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span class="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
-
-          {/* Button to add a new user */}
-          <button className="btn btn-outline-light">Add user</button>
+          <Link className="btn btn-outline-light" to="/add-user">
+            Add user
+          </Link>
         </div>
       </nav>
     </div>
